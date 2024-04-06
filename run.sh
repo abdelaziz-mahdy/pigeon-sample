1b2681f32d3d635a5d8a2edfe4d895db7d7853be
Abdelaziz Mahdy <zezohassam@gmail.com>
2024-04-06 21:36:23 +0200

    using .mm

---
+++ run.sh
@@ -0,0 +1,1 @@
+flutter pub run pigeon --input pigeons/message.dart --dart_out lib/pigeon.dart --objc_header_out ios/Runner/pigeon.h --objc_source_out ios/Runner/pigeon.mm --experimental_swift_out ios/Runner/Pigeon.swift --java_out ./android/app/src/main/kotlin/com/example/reproduce_issues_pigeon/Pigeon.java --java_package "com.example.reproduce_issues_pigeon"
